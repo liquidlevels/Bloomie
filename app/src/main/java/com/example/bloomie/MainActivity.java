17cf43c9f44b95dd3afd7847d5ea8c3453355f3b
Yuli-M <yuli.star.194@gmail.com>
2024-04-27 14:36:25 -0700

    referencias

--- app/src/main/java/com/example/bloomie/MainActivity.java
+++ app/src/main/java/com/example/bloomie/MainActivity.java
@@ -1,24 +1,62 @@
 package com.example.bloomie;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView id_temperatura;
+
+    private static final String TAG = "MainActivity"; // Define tu etiqueta de registro
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.id_suelo), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Write a message to the database
+        id_temperatura = findViewById(R.id.id_temperatura);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        // Lee desde la base de datos
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Este método se llama una vez con el valor inicial y nuevamente
+                // cada vez que los datos en esta ubicación se actualizan.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+                id_temperatura.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Error de lectura de datos
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
     }
 }
