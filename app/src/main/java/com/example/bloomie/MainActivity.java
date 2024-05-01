098eec9c5e62aa3c6cf83aefd15d1876a60e5129
david <al21760019@ite.edu.mx>
2024-04-30 22:46:26 -0700

    avance login y splash layout

--- app/src/main/java/com/example/bloomie/MainActivity.java
+++ app/src/main/java/com/example/bloomie/MainActivity.java
@@ -1,24 +1,40 @@
 package com.example.bloomie;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+      Animation animacion1= AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+      Animation animacion2= AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+      ImageView logo1= findViewById(R.id.logoFlores);
+      ImageView logo2= findViewById(R.id.logo);
+
+      logo1.setAnimation(animacion2);
+      logo2.setAnimation(animacion1);
+
+      new Handler().postDelayed(new Runnable() {
+          @Override
+          public void run() {
+              Intent intent =new Intent(MainActivity.this, loginActivity.class);
+              startActivity(intent);
+              finish();
+          }
+      }, 4000);
+
     }
 }
