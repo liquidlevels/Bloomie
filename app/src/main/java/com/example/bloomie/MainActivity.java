920c3a8507479492f110102c5395e113a9d2ba31
Yuli-M <yuli.star.194@gmail.com>
2024-05-04 11:28:51 -0700

    muestra temperatura y humedad de la bd

--- app/src/main/java/com/example/bloomie/MainActivity.java
+++ app/src/main/java/com/example/bloomie/MainActivity.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.widget.TextView;
 import android.view.WindowManager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -22,7 +23,10 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private TextView id_temperatura;
-    private DatabaseReference myRef;
+    private TextView id_humedad;
+    private TextView id_suelo;
+    private DatabaseReference dht11;
+    private DatabaseReference ground_humidity;
 
     private static final String TAG = "MainActivity"; // Define tu etiqueta de registro
     @Override
@@ -39,24 +43,51 @@ public class MainActivity extends AppCompatActivity {
 
         // Write a message to the database
         id_temperatura = findViewById(R.id.id_temperatura);
+        id_humedad = findViewById(R.id.id_humedad);
+        id_suelo = findViewById(R.id.id_suelo);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        myRef = FirebaseDatabase.getInstance().getReference().child("DHT11");
+        dht11 = FirebaseDatabase.getInstance().getReference().child("DHT11");
+        ground_humidity = FirebaseDatabase.getInstance().getReference().child("GROUND_HUMIDITY");
 
         // Lee desde la base de datos
-        myRef.addValueEventListener(new ValueEventListener() {
+        dht11.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // Este método se llama una vez con el valor inicial y nuevamente
                 // cada vez que los datos en esta ubicación se actualizan.
-                Float  value = dataSnapshot.child("humidity").child("value").getValue(Float.class);
+                Float  value = dataSnapshot.child("temperature").child("value").getValue(Float.class);
                 Log.d(TAG, "Value is: " + value);
                 id_temperatura.setText(value+"");
+
+                Float  valueh = dataSnapshot.child("humidity").child("value").getValue(Float.class);
+                Log.d(TAG, "Value is: " + valueh);
+                id_humedad.setText(valueh+"");
+
+                //Float  values = dataSnapshot.child("GROUND_HUMIDITY").child("value").getValue(Float.class);
+               // Log.d(TAG, "Value is: " + values);
+                //id_suelo.setText(values+"");
             }
 
             @Override
             public void onCancelled(DatabaseError error) {
                 // Error de lectura de datos
                 Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        ground_humidity.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //Float  values = dataSnapshot.child("GROUND_HUMIDITY").child("value").getValue(Float.class);
+                // Log.d(TAG, "Value is: " + values);
+                //id_suelo.setText(values+"");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Error de lectura de datos
+                Log.w(TAG, "Failed to read value.", error.toException());
+
             }
         });
 
--- app/src/main/java/com/example/bloomie/splash_activity.java
+++ app/src/main/java/com/example/bloomie/splash_activity.java
@@ -35,7 +35,7 @@ public class splash_activity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent =new Intent(splash_activity.this, loginActivity.class);
+                Intent intent =new Intent(splash_activity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
