405114fb432069fe38674d6940b49c3a8090e22c
liquid <al20760619@ite.edu.mx>
2024-05-12 18:03:42 -0700

    quite lo que no se usa, adapte nombres con el estandar general

--- backend/bloomie2/bloomie2.ino
+++ backend/bloomie2/bloomie2.ino
@@ -1,11 +1,3 @@
-/*
-  Fabrice Beya
-  The complete project details can be found here: 
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-*/
-
 #include <Arduino.h>
 #include <WiFi.h>
 #include <FirebaseESP32.h>
@@ -27,9 +19,6 @@
 // Initialize DHT sensor
 DHT dht(DHTPIN, DHTTYPE);
 
-// Device ID
-#define DEVICE_UID "4X"
-
 //MICASA
 //#define WIFI_SSID "Totalplay-C3AA-5G"
 //#define WIFI_PASSWORD "C3AA5AEFbJYcHgvZ"
@@ -49,10 +38,6 @@ DHT dht(DHTPIN, DHTTYPE);
 // Your Firebase Realtime database URL
 #define DATABASE_URL "https://bloomie-c2584-default-rtdb.firebaseio.com/" 
 
-// Constants
-const int VAL_HIGH = 1024; // DRY
-const int VAL_LOW = 800; // WET
-
 // Device Location config
 String device_location = "";
 
@@ -66,31 +51,26 @@ FirebaseAuth auth;
 FirebaseConfig config;
 
 // Firebase database path
-String dht11_path = "DHT11";
+String dht11_path = "dht11";
 String databasePath = "";
-String ground_humidity_path = "GROUND_HUMIDITY";
+String ground_humidity_path = "ground_humidity";
 
 // Firebase Unique Identifier
 String fuid = "";
 
 // Stores the elapsed time from device start up
-unsigned long elapsedMillis = 0;
+unsigned long elapsed_millis = 0;
 
 // The frequency of sensor updates to firebase, set to 10seconds
 unsigned long update_interval = 10000;
-
-// Dummy counter to test initial firebase updates
-int count = 0;
 
 // Store device authentication status
 bool isAuthenticated = false;
 
 // Variables to hold sensor readings
-//float temperature = 24.7;
-float temperature = 2;
-//float humidity = 60;
-float humidity = 30;
-int ground_humidity = 20;
+float temperature = 0;
+float humidity = 0;
+int ground_humidity = 0;
 
 // JSON object to hold updated sensor values to be sent to firebase
 FirebaseJson temperature_json;
@@ -148,8 +128,6 @@ void firebase_init() {
 }
 
 void groundhumidity_init(){
-  
-  //groundhumidity_begin();
   ground_humidity_json.add("name", "Ground Humidity");
   ground_humidity_json.add("value", ground_humidity);
 
@@ -168,24 +146,16 @@ void dhtt11_init(){
   dht.begin();
 
   // Initialise temprature json data
-  //temperature_json.add("deviceuid", DEVICE_UID);
   temperature_json.add("name", "Temperature");
-  //temperature_json.add("type", "Temperature");
-  //temperature_json.add("location", device_location);
   temperature_json.add("value", temperature);
+  // Initialise humidity json data
+  humidity_json.add("name", "Humidity");
+  humidity_json.add("value", humidity);
 
   // Print out initial temperature values
   String jsonStr;
   temperature_json.toString(jsonStr, true);
   Serial.println(jsonStr);
-
-  // Initialise humidity json data
-  //humidity_json.add("deviceuid", DEVICE_UID);
-  humidity_json.add("name", "Humidity");
-  //humidity_json.add("type", "Humidity");
-  //humidity_json.add("location", device_location);
-  humidity_json.add("value", humidity);
-
   String jsonStr2;
   humidity_json.toString(jsonStr2, true);
   Serial.println(jsonStr2);
@@ -231,26 +201,20 @@ void updateSensorReadings(){
 }
 
 void uploadSensorData() {
-  if (millis() - elapsedMillis > update_interval && isAuthenticated && Firebase.ready())
+  if (millis() - elapsed_millis > update_interval && isAuthenticated && Firebase.ready())
     {
-      elapsedMillis = millis();
+      elapsed_millis = millis();
 
       updateSensorReadings();
       String temperature_node = databasePath + dht11_path + "/temperature";  
       String humidity_node = databasePath + dht11_path + "/humidity"; 
       String ground_humidity_node = databasePath + ground_humidity_path + "/ground_humidity";
-      //String temperature_node = "/temperature";  
-      //String humidity_node = "/humidity"; 
 
       if (Firebase.setJSON(fbdo, temperature_node.c_str(), temperature_json))
       {
-          //Serial.println("PASSED");
           Serial.println("PATH: " + fbdo.dataPath());
-          //Serial.println("TYPE: " + fbdo.dataType());
-          //Serial.println("ETag: " + fbdo.ETag());
           Serial.print("VALUE: ");
           printResult(fbdo); //see addons/RTDBHelper.h
-          //Serial.println("------------------------------------");
           Serial.println();
       }
       else
@@ -263,13 +227,9 @@ void uploadSensorData() {
 
       if (Firebase.setJSON(fbdo, humidity_node.c_str(), humidity_json))
       {
-          //Serial.println("PASSED");
           Serial.println("PATH: " + fbdo.dataPath());
-          //Serial.println("TYPE: " + fbdo.dataType());
-          //Serial.println("ETag: " + fbdo.ETag());
           Serial.print("VALUE: ");
           printResult(fbdo); //see addons/RTDBHelper.h
-          //Serial.println("------------------------------------");
           Serial.println();
       }
       else
@@ -281,10 +241,7 @@ void uploadSensorData() {
       }
       
       if(Firebase.setJSON(fbdo,ground_humidity_node.c_str(), ground_humidity_json)){
-        //Serial.println("PASSED");
         Serial.println("PATH: " + fbdo.dataPath());
-        //Serial.println("TYPE: " + fbdo.dataType());
-        //Serial.println("ETag: " + fbdo.ETag());
         Serial.print("VALUE: ");
         printResult(fbdo); //see addons/RTDBHelper.h
         Serial.println("------------------------------------");
