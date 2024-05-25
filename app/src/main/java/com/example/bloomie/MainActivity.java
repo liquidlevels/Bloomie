7e2cc89b41ff48ee35f0a22db924eea3e964857b
Yuli-M <yuli.star.194@gmail.com>
2024-05-25 13:50:59 -0700

    acceso bd

--- app/src/main/java/com/example/bloomie/MainActivity.java
+++ app/src/main/java/com/example/bloomie/MainActivity.java
@@ -93,8 +93,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         id_suelo = findViewById(R.id.id_suelo);
         userID = findViewById(R.id.idUser);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        dht11 = FirebaseDatabase.getInstance().getReference().child("DHT11");
-        ground_humidity = FirebaseDatabase.getInstance().getReference().child("GROUND_HUMIDITY");
+        dht11 = FirebaseDatabase.getInstance().getReference().child("dht11");
+        ground_humidity = FirebaseDatabase.getInstance().getReference().child("ground_humidity");
         name = FirebaseDatabase.getInstance().getReference().child("Users");
         String id = mAuth.getCurrentUser().getUid();
         handler.postDelayed(updateDateTimeRunnable, 1000);
